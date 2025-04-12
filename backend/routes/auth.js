268065b5b9b0c0c62f3cd55770b428dc426d5fb7
SagnikGos <sagnik.gos06@gmail.com>
2025-04-13 02:23:09 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -97,8 +97,8 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
